97a3312ff45a249010d8ce39a598aa9591da8ad8
Andrew Coomes <ajc@ajcdrift.com>
2014-03-06 10:59:57 -0800

    fullPage.js
    Implemented fullPage.js (Thanks Alvaro!). Seems to be working well
    except for down arrow. Needs fixed or simply removed in favor of side
    nav.

--- load.js
+++ load.js
@@ -1,3 +1,14 @@
 $(document).ready(function () {
+    $.fn.fullpage({
+        resize: false,
+		slidesColor: ['#012', '#023', '#134'],
+        anchors: ['firstPage', 'secondPage', 'thirdPage'],
+        css3: true,
+        navigation: true, 
+        navigationTooltips: ['bio', 'exp', 'contact'],
+    });
+    $(".next-section-button").click(function() {
+        $.fn.fullpage.moveSectionDown();
+    });
     $(".connection-menu").delay(800).fadeIn(1000);
-});
+}); 
